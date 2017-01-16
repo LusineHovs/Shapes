9b12121c6c842e9aff95f4898c5e3730008e9002
Lusine Hovsepyan <lusine@hovsepyan.am>
2017-01-16 17:23:44 +0400

    Some changes

--- Shape/UserProgram/Program.cs
+++ Shape/UserProgram/Program.cs
@@ -12,9 +12,9 @@ namespace UserProgram
         static void Main(string[] args)
         {
             Ellipsoid elips = new Ellipsoid(10, 20, 70);
-            Console.WriteLine("The Volume of ellopsoid = {0}, the Surface of ellipsoid = {1}", elips.Volume, elips.Surface);
+            Console.WriteLine($"The Volume of ellopsoid = {elips.Volume}, the Surface of ellipsoid = {elips.Surface}");
             Parallelepiped piped = new Parallelepiped(20, 10, 20);
-            Console.WriteLine("The Volume of parallelepiped = {0}, the Surface of parallelepiped = {1}", piped.Volume, piped.Surface);
+            Console.WriteLine($"The Volume of parallelepiped = {piped.Volume}, the Surface of parallelepiped = {piped.Surface}");
             Console.Read();
         }
     }
